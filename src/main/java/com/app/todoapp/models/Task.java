22321d42fbb7b4eb06b090b22105cc67aa262347
Nikitaa19 <agarwalnikita064@gmail.com>
2025-03-18 22:49:31 +0530

    update

--- src/main/java/com/app/todoapp/models/Task.java
+++ src/main/java/com/app/todoapp/models/Task.java
@@ -1,8 +1,14 @@
 package com.app.todoapp.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue()
     private Long id;
     private String title;
     private boolean completed;
 }
-nikita
